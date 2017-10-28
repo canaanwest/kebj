fbc35008efac39b32ef582a97e3b839ae53aa4a7
Canaan West <canaan.west@gmail.com>
2017-10-27 23:47:51 -0700

    fix for serving photo to heroku?

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,9 @@ module Kebj
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    #to serve assets to heroku
+    config.serve_static_assets = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
